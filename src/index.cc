4cfaf5cbeab09f4c52e356136c6e2f7f9ed54298
mafanasiev <mafanasiev@tennisi.com>
2018-10-17 16:52:16 +0300

    1.0.0

--- src/index.cc
+++ src/index.cc
@@ -53,11 +53,6 @@ void Method2(const FunctionCallbackInfo<Value> &args)
   int x = (int)args[0]->NumberValue();
   double expValue = (double)args[1]->NumberValue();
 
-  if (!(expValue >= 0 && expValue <= 1))
-  {
-    isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Second argument must be between 0 and 1")));
-    return;
-  }
   double result = alglib_impl::poissondistribution(x, expValue, NULL);
   args.GetReturnValue().Set(v8::Number::New(isolate, result));
 }
